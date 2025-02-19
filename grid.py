f75dee96e655b08e3110e934d2050121501c3114
longson84 <nguyen.longson@gmail.com>
2025-02-19 21:08:37 +0700

    separate the grid class

--- grid.py
+++ grid.py
@@ -6,26 +6,84 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-class GridTrading():
+class Grid():
 
-    def __init__(self, prices, upper_range, lower_range, position_size=1e5, fee=0.01, num_grids=4, 
-                 grid_type='bull', 
-                 grid_levels_mode='linear', # linear = same absolute distance, percentage = same relative distance
+    def __init__(self, upper_range, lower_range, grid_levels_mode='linear'):
+        
+        self.upper_range = upper_range
+        self.lower_range = lower_range
+        self.grid_levels_mode = grid_levels_mode
+        self.grid_levels = []
+        
+    
+class Grid_Space(Grid):
+
+    def __init__(self, upper_range, lower_range, grid_space=10, grid_levels_mode='linear'):
+
+        super().__init__(upper_range, lower_range, grid_levels_mode)
+        self.grid_space = grid_space
+        self.grid_levels = self._get_grid_levels()
+
+    def _get_grid_levels(self):
+        if self.grid_levels_mode == 'linear':
+            return np.arange(start=self.lower_range, 
+                             stop=self.upper_range + self.grid_space, 
+                             step=self.grid_space).tolist()
+        elif self.grid_levels_mode == 'percentage':
+            space = 1 + 1e-2*self.grid_space
+            log_levels = np.arange(start=np.log(self.lower_range), 
+                                   stop=np.abs(np.log((space) * self.upper_range)), 
+                                   step=np.log(space))
+            return np.exp(log_levels).tolist()
+        else:
+            raise ValueError('grid_levels_mode must be either linear or percentage')
+        
+class Grid_Number(Grid):
+    
+    def __init__(self, upper_range, lower_range, num_grids=4, grid_levels_mode='linear'):
+
+        super().__init__(upper_range, lower_range, grid_levels_mode)
+
+        self.num_grids = num_grids
+        self.grid_levels = self._get_grid_levels()
+    
+    def _get_grid_levels(self):
+        if self.grid_levels_mode == 'linear':
+            return np.linspace(self.lower_range, self.upper_range, self.num_grids).tolist()
+        elif self.grid_levels_mode == 'percentage':
+            return np.geomspace(self.lower_range, self.upper_range, self.num_grids).tolist()
+        else:
+            raise ValueError("Invalid grid_levels_mode. Must be 'linear' or 'percentage'.")
+    
+
+
+class Grid_Trading():
+
+    def __init__(self, prices, 
+                 grid: Grid,
+                 position_size=1e5, 
+                 fee=0.1, # fee in percentage
                  sizing_mode='qty'):  # qty = same quantity, amount = same amount
 
-        if num_grids < 3:
-            raise ValueError("Number of grids must be at least 3.")
-        
         if len(prices) < 3:
             raise ValueError('There must be at least 3 price points.')
 
         self.prices = prices
         self.position_size = position_size
         self.fee = fee*1e-2
-        self.num_grids = num_grids
-        self.grid_type = grid_type.lower()
-        self.current_price = prices[0]
+        self.grid = grid
+        self.initial_price = prices[0]
 
+        self.upper_range = grid.upper_range
+        self.lower_range = grid.lower_range    
+
+        self.grid_levels_mode = grid.grid_levels_mode
+        self.grid_levels = grid.grid_levels
+        
+        self.sizing_mode = sizing_mode
+        self.amount = self.calculate_grid_amount() # will be used in case sizing_mode is 'amount'
+        self.qty = self.calculate_grid_qty() # will be used in case sizing_mode is 'qty'
+        
         # Tracking variables
         self.open_buy_orders = []  # List of lists for open buy orders at each step
         self.open_sell_orders = []  # List of lists for open sell orders at each step
@@ -35,48 +93,32 @@ class GridTrading():
         self.gross_realized = []  # Gross realized profit
         self.net_realized = []  # Net realized profit after fees
         self.floating = []  # Floating P/L (unrealized profit/loss)
+        self.invested = [] # amount invested at each step
 
-        self.upper_range = upper_range
-        self.lower_range = lower_range
-    
+    def update_state(self, 
+                     open_buy_orders, 
+                     open_sell_orders,
+                     transactions,
+                     fee, cash, grossed, net, floating, invested):
 
-        self.grid_levels_mode = grid_levels_mode
-        self.grid_levels = self.calculate_grid_levels()
-
-        logging.info(f'Grid levels: {self.grid_levels}')
+        self.open_buy_orders.append(open_buy_orders)
+        self.open_sell_orders.append(open_sell_orders)
+        self.transactions.append(transactions)
+        self.total_fee.append(fee)
+        self.cash.append(cash)
+        self.gross_realized.append(grossed)
+        self.net_realized.append(net)
+        self.floating.append(floating)
+        self.invested.append(invested)
         
-        self.sizing_mode = sizing_mode
 
-        self.amount = self.calculate_grid_amount() # will be used in case sizing_mode is 'amount'
-
-        logging.info(f'Grid amount: {self.amount}')
-
-        self.qty = self.calculate_grid_qty() # will be used in case sizing_mode is 'qty'
-
-        logging.info(f'Grid qty: {self.qty}')
-            
     def calculate_grid_qty(self):
-
-        levels_above = len([g for g in self.grid_levels if g > self.current_price])
-
-        s = sum([g for g in self.grid_levels if g < self.current_price]) + self.current_price * levels_above
-
+        levels_above = len([g for g in self.grid_levels if g > self.initial_price])
+        s = sum([g for g in self.grid_levels if g < self.initial_price]) + self.initial_price * levels_above
         return self.position_size / s
     
     def calculate_grid_amount(self):
-
         return self.position_size / (self.num_grids - 1)
-
-    def calculate_grid_levels(self):
-
-        if self.grid_levels_mode == 'linear':
-            return np.linspace(self.lower_range, self.upper_range, self.num_grids).tolist()
-
-        elif self.grid_levels_mode == 'percentage':
-            return np.exp(np.linspace(np.log(self.lower_range), np.log(self.upper_range), self.num_grids).tolist())
-        
-        else:  
-            raise ValueError('Invalid grid levels mode. Choose either "linear" or "percentage".')
     
     def closest_level_above(self, price):
         """Find the closest grid level above the given price"""
@@ -87,7 +129,6 @@ class GridTrading():
                    
     def closest_level_below(self, price):
         """Find the closest grid level below the given price"""
-
         if price <= self.lower_range:
             return 0
 
@@ -97,10 +138,8 @@ class GridTrading():
 
         if self.sizing_mode == 'qty':
             return {price: [self.qty, self.qty * price]}
-
         elif self.sizing_mode == 'amount':
             return {price: [self.amount / price, self.amount]}
-        
         else:
             raise ValueError('Invalid sizing mode. Choose either "qty" or "amount".')
         
@@ -111,13 +150,14 @@ class GridTrading():
 
         if self.sizing_mode == 'qty':
             return {'Buy': [price, self.qty, self.qty * price, at], 
-                    'Sell': [paired, self.qty, self.qty * paired, None]}
-        
+                    'Sell': [paired, self.qty, self.qty * paired, None],
+                    'Grossed': 0
+                    }        
         elif self.sizing_mode == 'amount':
             qty = self.amount / price
             return {'Buy': [price, qty, self.amount, at], 
-                    'Sell': [paired, qty, qty * paired, None]}
-
+                    'Sell': [paired, qty, qty * paired, None],
+                    'Grossed': 0}
         else:
             raise ValueError('Invalid sizing mode. Choose either "qty" or "amount".')
 
@@ -131,8 +171,8 @@ class GridTrading():
         fee = 0
         cash = self.position_size
 
-        levels_below = [level for level in self.grid_levels if level < self.current_price]
-        levels_above = [level for level in self.grid_levels if level > self.current_price]
+        levels_below = [level for level in self.grid_levels if level < self.initial_price]
+        levels_above = [level for level in self.grid_levels if level > self.initial_price]
 
         current_price = self.prices[0]
 
@@ -149,14 +189,17 @@ class GridTrading():
             fee += self.fee * trade['Buy'][2]
             cash -= trade['Buy'][2]
 
-        self.open_buy_orders.append(open_buy_orders)
-        self.open_sell_orders.append(open_sell_orders)
-        self.transactions.append(transactions)
-        self.total_fee.append(fee)
-        self.cash.append(cash)
-        self.gross_realized.append(0)
-        self.net_realized.append(-fee)
-        self.floating.append(0)
+        invested = self.position_size - cash
+
+        self.update_state(open_buy_orders=open_buy_orders,
+                          open_sell_orders=open_sell_orders,
+                          transactions=transactions,
+                          fee=fee, 
+                          cash=cash, 
+                          grossed=0, 
+                          neted=-fee, 
+                          floating=-fee, 
+                          invested=invested)
 
     
 
@@ -166,7 +209,7 @@ class GridTrading():
         open_buy_orders = copy.deepcopy(self.open_buy_orders[prev]) # list of dictionary {Price: [qty, amount]}
         open_sell_orders = copy.deepcopy(self.open_sell_orders[prev]) # list of dictionary {Price: [qty, amount]}
         transactions = copy.deepcopy(self.transactions[prev])
-        fee = 0
+        fee = self.total_fee[prev]
         cash = self.cash[prev]
 
         price = self.prices[idx]
@@ -203,18 +246,7 @@ class GridTrading():
                 # Placing the new buy order at the closest level below the filled sell order
                 new_buy_level = self.closest_level_below(level)
                 if new_buy_level > 0:
-
-                    if self.sizing_mode == 'qty':
-                        buy_qty = self.qty
-                    elif self.sizing_mode == 'amount':
-                        buy_qty = self.amount / new_buy_level
-                    else:
-                        raise ValueError('Invalid sizing mode')
-
-                    open_buy_orders.update({
-                        new_buy_level: [buy_qty, buy_qty * new_buy_level]
-                    })
-
+                    open_buy_orders.update(self.open_order_at_level(new_buy_level))
 
                 # Update the transaction list with the filled sell order
                 for trans in transactions:
@@ -223,6 +255,7 @@ class GridTrading():
                         fee += self.fee * trans['Sell'][2]
                         cash += trans['Sell'][2] * (1 - self.fee)
                         grossed += trans['Sell'][2] - trans['Buy'][2]
+                        trans['Grossed'] = grossed
                         break
             
         net = grossed - fee
@@ -243,15 +276,17 @@ class GridTrading():
 
         floating = price * holding_qty - holding_amount
 
-
-        self.open_buy_orders.append(open_buy_orders)
-        self.open_sell_orders.append(open_sell_orders)
-        self.transactions.append(transactions)
-        self.total_fee.append(self.total_fee[prev] + fee)
-        self.cash.append(cash)
-        self.gross_realized.append(grossed)
-        self.net_realized.append(net)
-        self.floating.append(floating)
+        self.update_state(
+            open_buy_orders=open_buy_orders,
+            open_sell_ỏders=open_sell_orders,
+            transactions=transactions,
+            fee=fee,
+            cash=cash,
+            grossed=grossed,
+            net=net,
+            floating=floating,
+            invested=holding_amount
+        )
 
     def run(self):
         """Run the grid trading simulation across all price points"""
@@ -265,30 +300,34 @@ class GridTrading():
 # %%
 import yfinance
 
+start_date = '2022-01-01'
+
+
 # Example usage
-MSFT = yfinance.download('MSFT', start='2022-01-01')[['Close']].squeeze().tolist()
-TSLA = yfinance.download('TSLA', start='2022-01-01')[['Close']].squeeze().tolist()
-AAPL = yfinance.download('AAPL', start='2022-01-01')[['Close']].squeeze().tolist()
-
-BTC = yfinance.download('BTC-USD', start='2024-08-01')[['Close']].squeeze().tolist()
-GOOGL = yfinance.download('GOOGL', start='2024-09-11')[['Close']].squeeze().tolist()
-
-
+MSFT = yfinance.download('MSFT', start=start_date)[['Close']].squeeze().tolist()
+# TSLA = yfinance.download('TSLA', start=start_date)[['Close']].squeeze().tolist()
+# AAPL = yfinance.download('AAPL', start=start_date)[['Close']].squeeze().tolist()
+# META = yfinance.download('META', start=start_date)[['Close']].squeeze().tolist()
+# GOOGL = yfinance.download('GOOGL', start=start_date)[['Close']].squeeze().tolist()
+# AMZN = yfinance.download('AMZN', start=start_date)[['Close']].squeeze().tolist()
 
 
+
+# BTC = yfinance.download('BTC-USD', start=start_date)[['Close']].squeeze().tolist()
+# ETH = yfinance.download('ETH-USD', start=start_date)[['Close']].squeeze().tolist()
 
 # %%
 import matplotlib.pyplot as plt
 from itertools import accumulate
 
 pos = 1e5
-grids = list(range(5, 50, 1))
-lower_range = 270
-upper_range = 600
 
-grids = [20]
+grid_number_list = [20]
 
 prices = MSFT
+
+lower_range = prices[0]*0.8
+upper_range = prices[0]*2
 
 # prices = BTC
 # lower_range = 50000
@@ -297,17 +336,20 @@ prices = MSFT
 
 print(f'Buy and Hold: {pos * (prices[-1]/prices[0] - 1)}')
 
-for grid in grids:
 
-    grid_trader = GridTrading(prices=prices, 
-                            lower_range=lower_range, 
-                            upper_range=upper_range, 
+for g in grid_number_list:
+
+    grid = Grid_Number(
+        lower_range=lower_range,
+        upper_range=upper_range,
+        num_grids=g,
+        grid_levels_mode='linear')
+
+    grid_trader = Grid_Trading(prices=prices,
                             position_size=pos,
+                            grid=grid,
                             fee=0,
-                            num_grids=grid, 
-                            grid_type='bull',
-                            grid_levels_mode='percentage',
-                            sizing_mode='qty')
+                            sizing_mode='amount')
 
     grid_trader.run()
 
@@ -322,7 +364,7 @@ for grid in grids:
 
     # Plotting net_realized, floating, and total on the left y-axis
     ax1.plot(x, net_realized, label='Net Realized')
-    ax1.plot(x, grid_trader.floating, label='Floating')
+    # ax1.plot(x, grid_trader.floating, label='Floating')
     ax1.plot(x, pl, label='Total')
     ax1.plot(x, buy_and_hold, label='Buy and Hold')
 
@@ -330,6 +372,7 @@ for grid in grids:
     ax1.set_ylabel('Net Realized, Floating, Total', color='tab:blue')
     ax1.tick_params(axis='y', labelcolor='tab:blue')
     ax1.grid(True)
+
 
     # Creating the right y-axis to plot the price (MSFT)
     # ax2 = ax1.twinx()
@@ -368,4 +411,4 @@ for grid in grids:
 
 
 # %%
-
+ 
